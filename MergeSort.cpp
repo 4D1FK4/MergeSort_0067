a47910ebdd745e81e41fb7c156c30e3e3a7951d8
4D1FK4 <adifkapersonal0812@gmail.com>
2026-04-02 10:29:05 +0700

    Input2

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -15,3 +15,20 @@ void input()
 
         if (n <= 20)
         {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+    cout << "\n------------------------------" << endl;
+    cout << "\nInputkan Isi element array" << endl;
+    cout << "\n------------------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index ke- " << i << " : ";
+        cin >> arr[i];
+    }
+}
